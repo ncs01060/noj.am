e1d6e9a51e3d4e91f20a981a1ef1e32d744991b6
Youngmin <ncs01045@gmail.com>
2024-10-29 19:07:31 +0900

    + 2739_구구단,10814_나이 순 정렬

--- 정렬/10814_나이 순 정렬re.py
+++ 정렬/10814_나이 순 정렬re.py
@@ -13,3 +13,12 @@ for i in sorted(player,key=lambda x: x[0]):
 '''
 
 
+n=int(input())
+player = []
+for i in range(n):
+    age,name = input().split()
+    player.append([int(age),name])
+
+for i in sorted(player, key=lambda x: x[0]):
+    print(*i)
+    
