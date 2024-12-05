89348acb66962b5b98ee5eba602e2236d6600b55
ncs01060 <codingmin@icloud.com>
2024-12-05 11:52:23 +0900

    [2024-12-05 1260 DFS/BFS basic]

--- BFS&DFS/1260_DFS와 BFS.py
+++ BFS&DFS/1260_DFS와 BFS.py
@@ -1,19 +1,43 @@
-def dfs(V):
-    print(g)
-    visited[V] = True  # 해당 V값 방문처리
-    print(V, end=" ")
-    for i in range(1, n + 1):
-        if not visited[i] and g[V][i]:  # 만약 i값을 방문하지 않았고 V와 연결이 되어 있다면
-            dfs(i)  # 해당 i 값으로 dfs를 돈다.(더 깊이 탐색)
-    
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
+from collections import deque
 n,m,v = map(int,input().split())
-g = [[0] * (n+1) for _ in range(n + 1)]
+metrix = [[0] * (n+1) for _ in range(n+1)]
+visited = [False] * (n+1)
+
 for i in range(m):
-    x,y=map(int,input().split())
-    g[x][y] = g[y][x] = 1
+    t,f = map(int,input().split())
+    metrix[t][f] = metrix[f][t] = 1
 
 
-visited = [False] * (len(g)+1)
-dfs(v)
+
+
+
+# 깊이 우선 탐색(그래프 이론)
+def dfs(metrix,visited,v):
+    stack = [v]
+    while stack:
+        value = stack.pop()
+        if not visited[value]:
+            print(value,end=' ')
+            visited[value] = True
+        for c in range(len(metrix)-1,-1,-1):
+            if metrix[value][c] == 1 and not visited[c]:
+                stack.append(c)
+        
+dfs(metrix,visited,v)
+print()
+def bfs(metrix,v,visited):
+    queue = deque()
+    queue.append(v)
+    while queue:
+        value = queue.popleft()
+        if not visited[value]:
+            print(value,end=' ')
+            visited[value] = True
+        for c in range(len(metrix[value])):
+            if metrix[value][c] == 1 and not visited[c]:
+                queue.append(c)
+
+visited = [False] * (n+1)
+bfs(metrix,v,visited)
+
+
